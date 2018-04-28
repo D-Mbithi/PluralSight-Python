3a53a62c7f922716edd0a9d392479351169bedfd
Dennis Mbithi <denmbithi@gmail.com>
2018-04-29 00:42:13 +0300

    Converted the console app to web app using flask

--- Python:Getting_started/student.py
+++ Python:Getting_started/student.py
@@ -5,9 +5,10 @@ class Student:
 
     school_name = "Springfield Elementary"
 
-    def __init__(self, name, student_id=323):
+    def __init__(self, name, last_name, student_id=323):
         self.name = name
         self.student_id = student_id
+        self.last_name = last_name
         students.append(self)
 
     def __str__(self):
@@ -18,4 +19,3 @@ class Student:
 
     def get_school_name(self):
         return self.school_name
-
