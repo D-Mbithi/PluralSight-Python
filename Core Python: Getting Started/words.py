d034d5f5fb7229abb07f3156792b62082aaec434
dennis mbithi <denmbithi@gmail.com>
2022-02-18 23:22:01 +0300

    Added doc string

--- Core Python: Getting Started/words.py
+++ Core Python: Getting Started/words.py
@@ -1,8 +1,22 @@
+"""Retrieves and prints words from a documentself.
+
+    Usage:
+        python words.py 'http://sixty-north.com/c/t.txt'
+"""
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    """Fetch a list fo words from a URL.
+
+        Args:
+            url: The url of utf-8 deocument.
+
+        Returns:
+            A list of string containing words from the document.
+
+    """
     story = urlopen(url)
 
     story_words = []
@@ -19,11 +33,22 @@ def fetch_words(url):
 
 
 def print_items(story_words):
+    """ Prints items one per line.
+
+        Args:
+            A iterable series of printable items.
+
+    """
     for word in story_words:
         print(word)
 
 
 def main(url):
+    """Prints each word in one line.
+
+        Args:
+            url: The url of a utf-8 document.
+    """
     words = fetch_words(url)
 
     print_items(words)
